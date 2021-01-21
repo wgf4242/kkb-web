e3af36d5b318a888d7b3f38d4ef7ebea999512c9
wgf4242 <wgf4242@gmail.com>
2021-01-21 22:05:50 +0800

    lesson10

---
+++ lesson10/src/index.js
@@ -0,0 +1,13 @@
+// import * as React from "react";
+import * as ReactDOM from "react-dom";
+import SetStatePage from "./pages/SetStatePage";
+
+import "./index.css";
+
+import App from "./App";
+// import jsx from "./pages/ExamplePage";
+
+// ReactDOM.render(jsx, document.getElementById("root"));
+ReactDOM.render(<SetStatePage />, document.getElementById("root"));
+
+// console.log("React", React.version); //sy-log
