e3352e029c4e98ffd08ac26ae7f72f528f092942
wgf4242 <wgf4242@gmail.com>
2021-02-04 13:08:54 +0800

    sync

--- ex06/index.js
+++ ex06/index.js
@@ -1,22 +1,24 @@
 const crypto = require('crypto')
 module.exports.createToken = token => {
-    const ary = token.split('.')
-    if (ary.length !== 3) {
-        return
-    }
+  const ary = token.split(".");
+  if (ary.length !== 3) {
+    return;
+  }
 
-    return {
-        getExp: () => {
-            // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgywgywgywgywgdqgdUgdvgdegdwgywgdYgdggRcgdPgdUgdggdcgywgcRgywgcwg9kg9cg9qgqDgdYgdggdmgdegd9gqRgdvgd9gRygywgcPgd=gddgddgd9gdmgqDgddgdmgdUgdDgqRgdggdmgRcg9YgqYg9mgqlgywgqdgdygdggdegd9gq=gqegqdgqkgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywgdYgdggRcgdPgdUgdggdcgqDgd9gRqgdY
-            // ##END##
-        },
+  return {
+    getExp: () => {
+      const [a, b, c] = token.split(".");
+      const obj_str = Buffer.from(b, "base64").toString("ascii");
+      const data = JSON.parse(obj_str);
+      return data.exp;
+    },
 
-        verify: key => {
-            const hmac = crypto.createHmac('SHA256', key).update(ary[0]+ '.' +  ary[1]).digest('base64');
-            return hmac === ary[2] + '='
-            
-        }
-    }
+    verify: key => {
+      const hmac = crypto
+        .createHmac("SHA256", key)
+        .update(ary[0] + "." + ary[1])
+        .digest("base64");
+      return hmac === ary[2] + "=";
+    },
+  };
 }
