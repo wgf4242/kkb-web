12a0b9b93ad43fef73e754f14eb4d1eccd8e30f9
wgf4242 <wgf4242@gmail.com>
2021-06-08 08:29:36 +0800

    Added: 8-2 38-粘贴和拖拽图片

--- front/pages/editor/new.vue
+++ front/pages/editor/new.vue
@@ -5,7 +5,7 @@
     </div>
     <el-row>
       <el-col :span="12">
-        <textarea class="md-editor" :value="content" @input="update" ></textarea>
+        <textarea ref="editor" class="md-editor" :value="content" @input="update" ></textarea>
       </el-col>
       <el-col :span="12">
         <div class="markdown-body" v-html="compiledContent"></div>
@@ -26,14 +26,34 @@ export default {
       `
     };
   },
+  mounted() {
+    this.timer = null
+    this.bindEvents();
+  },
   computed: {
     compiledContent() {
       return marked(this.content, {});
     }
   },
   methods: {
+    bindEvents() {
+      this.$refs.editor.addEventListener('paste', async e=>{
+        const files = e.clipboardData.files
+        console.log(files);
+        // 文件上传
+      })
+      this.$refs.editor.addEventListener('drop', async e=>{
+        const files = e.dataTransfer.files
+        console.log(files);
+        // @todo 文件上传
+        e.preventDefault();
+      })
+    },
     update(e) {
-      this.content = e.target.value;
+      clearTimeout(this.timer)
+      setTimeout(() => {
+          this.content = e.target.value;
+      }, 350)
     },
     submit() {
 
